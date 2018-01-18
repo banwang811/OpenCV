cb2e818179809b034a2b7e309c205b92665e6248
banwang811 <759919341@qq.com>
2018-01-18 17:42:34 +0800

    init

---
+++ OpenCV/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenCV
+//
+//  Created by banwang on 2018/1/18.
+//  Copyright © 2018年 guanqun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenCV/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenCV
+//
+//  Created by banwang on 2018/1/18.
+//  Copyright © 2018年 guanqun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
